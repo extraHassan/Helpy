e97d6b16a754f08afe35def60ea306d68f3e6a2b
Ahmed <belarbi.ahmed21@gmail.com>
2018-12-30 19:51:14 +0100

    before pulling hassan

--- src/dao/PrayerDaoImpl.java
+++ src/dao/PrayerDaoImpl.java
@@ -1,6 +1,9 @@
 package dao;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Vector;
 
 import dao.consts.DatabaseInfos;
 import models.Prayer;
@@ -22,19 +25,37 @@ public class PrayerDaoImpl implements PrayerDao {
 
 	@Override
 	public Prayer select(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Prayer prayer = new Prayer();
+		String[][] data = db.select(DatabaseInfos.PRAYER, "id", id);
+		for (int i = 1; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				// System.out.println(data[i][j]);
+				prayer.setId(Integer.parseInt(data[i][j]));
+				prayer.setArabicName(data[i][++j]);
+				prayer.setFrenchName(data[i][++j]);
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+				LocalTime dateTime = LocalTime.parse(data[i][++j], formatter);
+				prayer.setTime(dateTime);
+				prayer.setNotificationMessage(data[i][++j]);
+
+			}
+		}
+		return prayer;
 	}
 
 	@Override
 	public boolean insert(Prayer model) {
-		// TODO Auto-generated method stub
+		int a = db.insert(model);
+		if (a != 0)
+			return true;
 		return false;
 	}
 
 	@Override
 	public boolean delete(int id) {
-		// TODO Auto-generated method stub
+		int a = db.delete(DatabaseInfos.PRAYER, "id", id);
+		if (a != 0)
+			return true;
 		return false;
 	}
 
@@ -46,10 +67,32 @@ public class PrayerDaoImpl implements PrayerDao {
 
 	@Override
 	public List<Prayer> liste() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Prayer> prayers = new Vector<>();
+		Prayer prayer = new Prayer();
+		String[][] data = db.select(DatabaseInfos.PRAYER);
+		for (int i = 1; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				// System.out.println(data[i][j]);
+				prayer.setId(Integer.parseInt(data[i][j]));
+				prayer.setArabicName(data[i][++j]);
+				prayer.setFrenchName(data[i][++j]);
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+				LocalTime dateTime = LocalTime.parse(data[i][++j], formatter);
+				prayer.setTime(dateTime);
+				prayer.setNotificationMessage(data[i][++j]);
+				prayers.add(prayer);
+
+			}
+		}
+		return prayers;
 	}
 
-
+	@Override
+	public boolean updateTime(int id, String name) {
+		int a = db.update(DatabaseInfos.PRAYER, id, name);
+		if (a != 0)
+			return true;
+		return false;
+	}
 
 }
